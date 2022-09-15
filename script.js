6d9e8da19324bdb8dcab0e8b86b74a2aba09a478
pauljsyi <pauljsyi@gmail.com>
2022-09-15 14:31:50 -0700

    added javascript functions for changing login button text to logout, removing add definition button when clicked, and alerting user when clicking like button

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 // Change the text to say "Logout" when clicking the "Login" button
-// Remote the Add Definition button when it is clicked
+
+function logout(e) {
+  // console.log(e);
+  // console.log(e.innerText);
+  if (e.innerText != "Logout") {
+    return (e.innerText = "Logout");
+  } else {
+    return (e.innerText = "Login");
+  }
+}
+
+// Remove the Add Definition button when it is clicked
+
+function removeButton(e) {
+  return e.remove();
+}
 
 // Alert "Ninja was liked" when click a like button
+
+function likeButton(e) {
+  console.log(e);
+}
+
+function alertLike() {
+  return alert("Ninja was liked");
+}
